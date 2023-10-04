5740ad4f010bc7530d57c1c05f96b7b154914400
Robin <robin.lichtenthaeler@web.de>
2023-10-04 17:22:53 +0200

    added literature

--- src/core/qualitymodel/literature.ts
+++ src/core/qualitymodel/literature.ts
@@ -5,5 +5,82 @@ export const literature = {
         "authors": "Cornelia Davis",
         "publisher": "Manning",
         "url": "https://www.manning.com/books/cloud-native-patterns"
+    },
+    "Garrison2017": {
+        "title": "Cloud Native Infrastructure",
+        "year": 2017,
+        "authors": "Justin Garrison and Kris Nova",
+        "publisher": "O'Reilly",
+        "url": "https://www.oreilly.com/library/view/cloud-native-infrastructure/9781491984291/"
+    },
+    "Bastani2017": {
+        "title": "Cloud Native Java",
+        "year": 2017,
+        "authors": "Kenny Bastani and Josh Long",
+        "publisher": "O'Reilly",
+        "url": "https://www.oreilly.com/library/view/cloud-native-java/9781449374631/"
+    },
+    "Scholl2019": {
+        "title": "Cloud Native",
+        "year": 2019,
+        "authors": "Boris Scholl, Trent Swanson, and Peter Jausovec",
+        "publisher": "O'Reilly",
+        "url": "https://www.oreilly.com/library/view/cloud-native/9781492053811/"
+    },
+    "Richardson2019": {
+        "title": "Microservices Patterns",
+        "year": 2019,
+        "authors": "Chris Richardson",
+        "publisher": "Manning",
+        "url": "https://www.manning.com/books/microservices-patterns"
+    },
+    "Reznik2019": {
+        "title": "Cloud Native Transformation",
+        "year": 2019,
+        "authors": "Pini Reznik, Jamie Dobson, and Michelle Gienow",
+        "publisher": "O'Reilly",
+        "url": "https://www.oreilly.com/library/view/cloud-native-transformation/9781492048893/"
+    },
+    "Arundel2019": {
+        "title": "Cloud Native DevOps with Kubernetes",
+        "year": 2019,
+        "authors": "John Arundel and Justin Domingus",
+        "publisher": "O'Reilly",
+        "url": "https://www.oreilly.com/library/view/cloud-native-devops/9781492040750/"
+    },
+    "Ibryam2020": {
+        "title": "Kubernetes Patterns",
+        "year": 2020,
+        "authors": "Bilgin Ibryam and Roland Huß",
+        "publisher": "O'Reilly",
+        "url": "https://www.oreilly.com/library/view/kubernetes-patterns/9781492050278/"
+    },
+    "Adkins2020": {
+        "title": "Building Secure and Reliable Systems",
+        "year": 2020,
+        "authors": "Heather Adkins, Betsy Beyer, Paul Blankinship, Piotr Lewandowski, Ana Oprea, and Adam Stubblefield",
+        "publisher": "O'Reilly",
+        "url": "https://www.oreilly.com/library/view/building-secure-and/9781492083115/"
+    },
+    "Indrasiri2021": {
+        "title": "Design Patterns for Cloud Native Applications",
+        "year": 2021,
+        "authors": "Kasun Indrasiri and Sriskandarajah Suhothayan",
+        "publisher": "O'Reilly",
+        "url": "https://www.oreilly.com/library/view/design-patterns-for/9781492090700/"
+    },
+    "Ruecker2021": {
+        "title": "Practical Process Automation",
+        "year": 2021,
+        "authors": "Bernd Rücker",
+        "publisher": "O'Reilly",
+        "url": "https://www.oreilly.com/library/view/practical-process-automation/9781492061441/"
+    },
+    "Goniwada2021": {
+        "title": "Cloud Native Architecture and Design",
+        "year": 2021,
+        "authors": "Shivakumar Goniwada",
+        "publisher": "Apress",
+        "url": "https://link.springer.com/book/10.1007/978-1-4842-7226-8"
     }
 }
