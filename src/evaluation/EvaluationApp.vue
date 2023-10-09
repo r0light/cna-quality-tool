c538fbc8424f8ef337709f118544b02f83b2676e
Robin <robin.lichtenthaeler@web.de>
2023-10-09 14:49:01 +0200

    added dummy metric

--- src/evaluation/EvaluationApp.vue
+++ src/evaluation/EvaluationApp.vue
@@ -7,6 +7,10 @@
                 <option v-for="system of systemsData" :value="system.index">{{ system.name }}</option>
             </select>
         </div>
+        <div v-if="selectedSystemIndex > -1">
+            <p>In development...</p>
+            <p>Number of services:<span> {{ getNumberOfServices(selectedSystemIndex) }} </span></p>
+        </div>
     </div>
 </template>
 
@@ -23,20 +27,21 @@ const selectedSystemIndex = ref<number>(-1)
 
 function onSelectSystem() {
 
-    if (selectedSystemIndex.value == -1) {
+    // start metric calculation here?
+}
+
+function getNumberOfServices(systemIndex: number) {
+
+    if (systemIndex === -1) {
         return;
     }
 
-    let selectedSystem = props.systemsData.find(system => system.index === selectedSystemIndex.value);
+    let selectedSystem = props.systemsData.find(system => system.index === systemIndex);
     let systemEntityManager = toRaw(selectedSystem.entityManager);
 
     let services = [...systemEntityManager.getSystemEntity().getComponentEntities.entries()].map(entry => entry[1]).filter(entity => entity.constructor.name === "Service");
 
-    /*
-    console.log({"measure": "number of services",
-                "value:": services.length
-            })
-            */
+    return services.length;
 }
 
 </script>
