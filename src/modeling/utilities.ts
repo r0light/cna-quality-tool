343ed0633015c2bfce6d4fd3bb8a07f3a5992869
Robin <robin.lichtenthaeler@web.de>
2024-09-26 11:54:30 +0200

    refactor download

---
+++ src/modeling/utilities.ts
@@ -0,0 +1,62 @@
+import { dia } from '@joint/core';
+import { addSelectionToolToEntity } from "./views/tools/entitySelectionTools";
+
+export function ensureCorrectRendering(createdCells: dia.Cell[], paper: dia.Paper): Promise<void> {
+    return new Promise<void>((outerResolve, outerReject) => {
+
+        let cellsRendered = [];
+
+        for (const cell of createdCells) {
+
+            let cellRendered = Promise.resolve();
+
+            if (cell.isElement()) {
+
+                // resize element to a different size and that to the wanted size again, to rerender the bounding box and ensure that it has the right size
+                let wantedWidth = cell.prop("size/width");
+                let wantedHeight = cell.prop("size/height");
+                cellsRendered.push(
+                    cellRendered.then(() => {
+                        return new Promise<void>((resolve, reject) => {
+                            //element.resize(element.prop("defaults/size").width, element.prop("defaults/size").height);
+                            (cell as dia.Element).resize(wantedWidth + 10, wantedHeight + 10);
+                            setTimeout(() => {
+                                resolve();
+                            }, 100)
+                        })
+                    }).then(() => {
+                        return new Promise<void>((resolve, reject) => {
+                            (cell as dia.Element).resize(wantedWidth, wantedHeight);
+                            setTimeout(() => {
+                                resolve();
+                            }, 100)
+                        });
+                    }).then(() => {
+                        return new Promise<void>((resolve, reject) => {
+                            addSelectionToolToEntity(paper.requireView(cell as dia.Element).model, paper);
+                            setTimeout(() => {
+                                resolve();
+                            }, 100)
+                        });
+                    })
+                );
+            } else if (cell.isLink) {
+                (cell.findView(paper) as dia.LinkView).requestConnectionUpdate();
+            } 
+        }
+
+        Promise.all(cellsRendered).then(() => {
+            //mainPaper.value.updateViews();
+            paper.hideTools();
+            outerResolve();
+        })
+    });
+}
+
+export function triggerDownload(content: string, mediaType: string, fileName: string) {
+    // download (taken from https://stackoverflow.com/a/22347908)
+    let downloadElement = document.createElement("a");
+    downloadElement.setAttribute('href', `data:${mediaType};charset=utf-8,` + encodeURIComponent(content));
+    downloadElement.setAttribute('download', fileName);
+    downloadElement.click();
+}
